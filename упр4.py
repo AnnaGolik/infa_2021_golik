700fee10f778d2e99c7637a510db828293957669
AnnaGolik <91071459+AnnaGolik@users.noreply.github.com>
2022-03-16 10:11:28 +0300

    Update упр4.py

--- упр4.py
+++ упр4.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 with plt.xkcd():
-    x = np.arange(-10, 10, 0.1)
+    x = np.arange(-10, 10, 0.4)
     plt.figure(figsize=(9, 6))
+    sp = plt.subplot(111)
+    sp.spines['left'].set_position('center')
+    sp.spines['bottom'].set_position('center')
+    sp.spines['top'].set_position('center')
+    sp.spines['right'].set_position('center')
     plt.plot(x, eval(input()))
     plt.show()
