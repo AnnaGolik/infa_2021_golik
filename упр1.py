beebca7bf2cae367c9b06b0c2dafaecd4dfdf7e3
AnnaGolik <91071459+AnnaGolik@users.noreply.github.com>
2022-03-18 18:46:54 +0300

    Update упр1.py

--- упр1.py
+++ упр1.py
@@ -4,7 +4,7 @@ def sentence(x):
     for i in range(3):
         a = (np.log((1/np.e**(np.sin(x[i])+1))/(5/4+1/x[i]**15))/np.log(1 + x[i]**2))
         print(a)
-    return b
+    return a
 
 x = [1, 10, 1000]
 y=sentence(x)
